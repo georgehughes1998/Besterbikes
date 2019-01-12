e3e65ff58e047baa7e2bac0e27485f38e18674d2
jackow98 <jw50@hw.ac.uk>
2019-01-12 21:47:46 +0000

    Started to add redux-form, unfinished

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,53 +1,73 @@
 import React from 'react'
+import {Field, reduxForm} from 'redux-form'
 
-const SignIn = () =>{
-    return(
-        <div>
+class SignIn extends React.Component{
+    renderInput = ({input, label}) => {
 
-             {/*Besterbikes Logo */}{/*TODO Get new logo and put here*/}
-            <img className="ui centered medium image"
-                 alt="Besterbikes Logo"
-                 src="https://lh6.googleusercontent.com/jsF13Ay0tZSL5qr7Bcoj1H84O9tePF-U0XsC3Z2mRikOFkE9Bf-lmppDB-V-Kf32etoUP-Aw2vcTXrp2lNqT=w1920-h903"
-            />
-
-            {/*Divider Line*/}
-            <div className="ui horizontal divider">
-                Log In
-            </div>
-
-            {/*Log In Form*/}
-            <div className="ui container middle aligned center aligned grid">
-                <div className="column">
-
-                    <div className="ui container">
-                        <div className="ui input">
-                            <input type="text" placeholder="Email Address"/>
-                        </div>
-                    </div>
-
-                    <br/>
-
-                    <div className="ui container">
-                        <div className="ui input">
-                            <input type="text" placeholder="Password"/>
-                        </div>
-                    </div>
-
-                    <br/>
-
-                    <button className="ui big primary button">
-                        Log In
-                    </button>
-
-
-                    <button className="ui big button">
-                        Sign Up
-                    </button>
-
+        return(
+            <div className="ui container">
+                <div className="ui input">
+                    <label>{label}</label>
+                    <input {...input}/>
                 </div>
             </div>
-        </div>
-    )
-};
+            )
+    };
 
-export default SignIn
+    onSubmit(formValues){
+        console.log(formValues)
+    }
+
+    render(){
+        return(
+            <div>
+
+                {/*Besterbikes Logo */}{/*TODO Get new logo and put here, GDRIVE not working on mobile*/}
+                <img className="ui centered medium image"
+                     alt="Besterbikes Logo"
+                     src="https://lh6.googleusercontent.com/jsF13Ay0tZSL5qr7Bcoj1H84O9tePF-U0XsC3Z2mRikOFkE9Bf-lmppDB-V-Kf32etoUP-Aw2vcTXrp2lNqT=w1920-h903"
+                />
+
+                {/*Divider Line*/}
+                <div className="ui horizontal divider">
+                    Log In
+                </div>
+
+                {/*Log In Form*/}
+                <form className="ui container middle aligned center aligned grid" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                    <div className="column">
+
+                        <Field>
+                            name = "email"
+                            component = {this.renderInput}
+                            label = "Email:"
+                        </Field>
+
+                        <Field>
+                            name = "password"
+                            component = {this.renderInput}
+                            label = "Password:"
+                        </Field>
+
+
+                        <br/>
+
+                        <button className="ui big primary button">
+                            Log In
+                        </button>
+
+
+                        <button className="ui big button">
+                            Sign Up
+                        </button>
+
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default reduxForm({
+    form: 'signIn'
+})(SignIn);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 
 import Firebase, { FirebaseContext } from './components/Firebase'
+
 import App from "./components/App";
 import reducers from "./reducers";
-
 
 //Hooks up Redux development tool
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
