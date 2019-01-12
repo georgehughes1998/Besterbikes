5fbd0338176fc9beff4a6640265d8c9cb1a9520b
jackow98 <jw50@hw.ac.uk>
2019-01-12 18:13:46 +0000

    Added header component to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import MainMenu from './MainMenu'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
+import Header from "./Header";
 
 const App = () => {
     return(
         <div>
             <BrowserRouter>
                 <div>
+                    <Header/>
                     <Route path="/" exact component={MainMenu}/>
                     <Route path="/signin" exact component={SignIn}/>
                     <Route path="/signup" exact component={SignUp}/>
