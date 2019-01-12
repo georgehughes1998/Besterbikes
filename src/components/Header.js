7f3ba8bac0fb746fb05876450532b176dc17f0d2
jackow98 <jw50@hw.ac.uk>
2019-01-12 18:53:49 +0000

    Added semantic Ui capabilities

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+//TODO: Make pretty
 const Header = () => {
     return(
-        <div>
-            Header
+        <div className="ui inverted blue three item menu">
+            <a className="item">Back button</a>
+            <a className="item">Title of Page</a>
+            <a className="item">Logo</a>
         </div>
     )
 };
