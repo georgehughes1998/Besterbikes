b4deba4ab34d11e923e0a337992c2b181eabff84
jackow98 <jw50@hw.ac.uk>
2019-01-20 02:18:11 +0000

    Implemented Steps through reservation flow to update as fields completed, needs to be commeneted and optimised

--- src/components/reservationHandling/Steps.js
+++ src/components/reservationHandling/Steps.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom'
 import {Icon, Step} from "semantic-ui-react";
-import {reduxForm} from "redux-form";
+import {reduxForm, formValueSelector } from "redux-form";
 import validate from "./validate";
+import connect from "react-redux/es/connect/connect";
 
-const Steps = (props) => {
+let Steps = (props) => {
 
     //Change functionality of clicking steps to work with redux form
     const handleClick = (link) => props.history.push(link);
+
+    //TODO: Implment complete
+    const isComplete = (key) => {
+        console.log(key);
+        switch (key.title) {
+            case "Select Bikes":
+                return props.selectedBike.station && props.selectedBike.regularBikes && props.selectedBike.mountainBikes;
+            case "Date and Time":
+                return props.selectedDateAndTime.startTime && props.selectedDateAndTime.startDate;
+            case "Review Order":
+                return props.selectedBike.station;
+            case "Payment":
+                return props.paid.cardNumber && props.paid.CVV && props.paid.country && props.paid.expirationDate;
+
+
+        }
+    };
+
 
     const renderSteps = (Object.values(props.steps).map((key, index) => {
         return (
@@ -15,7 +34,7 @@ const Steps = (props) => {
                 key={index}
                 link
                 onClick={() => handleClick(key.link)}
-                completed = {props.station && props.mountainBikes && props.regularBikes}
+                completed = {isComplete(key)}
             >
                 <Icon name={key.icon}/>
                 <Step.Content>
@@ -24,7 +43,7 @@ const Steps = (props) => {
                 </Step.Content>
             </Step>
         )
-    }))
+    }));
 
     return (
         <Step.Group
@@ -36,10 +55,28 @@ const Steps = (props) => {
             {renderSteps}
         </Step.Group>
     )
-}
+};
 
-export default withRouter(reduxForm({
-    form: 'reservebike', //Form name is same
-    destroyOnUnmount: false,
-    validate
-})(Steps))
+// Decorate with redux-form
+Steps = reduxForm({
+    form: 'reservebike' // a unique identifier for this form
+})(Steps);
+
+// Decorate with connect to read form values
+const selector = formValueSelector('reservebike') ;// <-- same as form name
+Steps = connect(state => {
+    // can select values individually
+    const selectedBike = selector(state, 'station', 'mountainBikes', 'regularBikes');
+    const selectedDateAndTime = selector(state, 'startDate', 'startTime');
+    //TODO: Add conformiation
+    const paid = selector(state, 'cardNumber', 'CVV', 'expirationDate', 'country');
+
+
+    return {
+        selectedBike,
+        selectedDateAndTime,
+        paid
+    }
+})(Steps);
+
+export default withRouter(Steps)
