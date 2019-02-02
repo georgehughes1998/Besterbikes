7fb4a3fe9929aab850830fd22808eaed91f04b8a
jackow98 <jw50@hw.ac.uk>
2019-02-02 20:56:50 +0000

    Got map markers rendering based on JSON, struglging to load into dropdown menu

--- src/components/reservationHandling/SelectBikes.js
+++ src/components/reservationHandling/SelectBikes.js
@@ -5,61 +5,95 @@ import validate from './validate'
 import ReservationHandlingForm from "./ReservationHandlingForm";
 import BesterbikesMap from '../map/BesterbikesMap'
 import {Container, Segment} from "semantic-ui-react";
+import {getJSONFromFile} from "../../handleJSON";
 
-
+//TODO: Render values in dropdown
 // TODO: Move map into segment and fix diaplsy issues
 //Component that passes relevant fields to ReservationHandlingForm for a reserving a bike process
-const SelectBikes = (props) => {
+class SelectBikes extends React.Component{
 
-    return (
-        <Segment.Group>
+    constructor(props) {
+        super(props);
+        this.state = {
+            options: ""
+        }
+    }
 
-            <Segment style={{minHeight:"300px"}}>
+    componentDidMount(){
+        if((this.state.options === "")){
+            this.getStations()
+        }
+    }
+
+    getStations = async () =>{
+        const stations = JSON.parse(await getJSONFromFile("/JSONFiles/stations.json"));
+        // console.log("MAP JSON RETRIVED");
+        const options =  Object.values(stations).map((key, index) => ({key: index, value: index, text : key.name}));
+        this.setState({options});
+        console.log(this.state.options);
+    };
+
+    render(){
+        return (
+            <Segment.Group>
+
+                <Segment style={{minHeight:"300px"}}>
                     <div>
-                        <BesterbikesMap/>
+                        <BesterbikesMap style={{
+                            maxWidth: "100%",
+                            maxHeight: "100%",
+                            marginRight: "14px",
+                            marginLeft: "-14px",
+                            marginTop: "-14px",
+                            marginBottom: "20px",
+                            position: "absolute"
+                        }}/>
                     </div>
-            </Segment>
+                </Segment>
 
-            <ReservationHandlingForm
-                header={{
-                    title: "Select Bikes",
-                    description: "Choose your start station and number of bikes",
-                    progress: 20,
-                    icon: "bicycle"
-                }}
+                <ReservationHandlingForm
+                    header={{
+                        title: "Select Bikes",
+                        description: "Choose your start station and number of bikes",
+                        progress: 20,
+                        icon: "bicycle"
+                    }}
 
-                fields={{
-                    station: {
-                        name: 'station',
-                        label: 'Station',
-                        type: 'dropdown'
-                    },
-                    mountainBikes: {
-                        name: 'mountainBikes',
-                        label: 'Mountain Bikes',
-                        type: 'number'
-                    },
-                    regularBikes: {
-                        name: 'regularBikes',
-                        label: 'Regular Bikes',
-                        type: 'number'
-                    },
+                    fields={{
+                        station: {
+                            name: 'station',
+                            label: 'Station',
+                            type: 'dropdown',
+                            values: this.state.options
+                        },
+                        mountainBikes: {
+                            name: 'mountainBikes',
+                            label: 'Mountain Bikes',
+                            type: 'number'
+                        },
+                        regularBikes: {
+                            name: 'regularBikes',
+                            label: 'Regular Bikes',
+                            type: 'number'
+                        },
 
-                }}
+                    }}
 
-                operations={{
-                    next: {
-                        link: props.onSubmit,
-                        type: 'submit',
-                        className: 'next',
-                        color: 'green',
-                        text: 'Next'
-                    }
-                }}
-            />
+                    operations={{
+                        next: {
+                            link: this.props.onSubmit,
+                            type: 'submit',
+                            className: 'next',
+                            color: 'green',
+                            text: 'Next'
+                        }
+                    }}
+                />
 
-        </Segment.Group>
-    )
+            </Segment.Group>
+        )
+    }
+
 
 };
 
