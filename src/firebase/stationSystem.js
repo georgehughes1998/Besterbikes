410ce63db577eb352c1bbf7659d8fd6fb0d63b69
George <hughesgeorgem@gmaill.com>
2019-02-06 21:42:55 +0000

    Begun implementation of stationSystem.js, specifically unlockBike

--- src/firebase/stationSystem.js
+++ src/firebase/stationSystem.js
@@ -1,17 +1,63 @@
-export const unlockBike = (stationID, reservationID) => {
-    //TODO selects and unlocks a bike, marking the reservations as active
+import * as firebase from "firebase";
 
-    //TODO set bike status to be in-use
-    //TODO update associatedReservation on bike to be the reservationID
-    return 0;
+
+
+export const unlockBike = async (stationID, reservationID) => {
+
+    const db = firebase.firestore();
+
+    const stationsCollection = db.collection('station');
+    const stationDocument = stationsCollection.doc(stationID);
+
+    const reservationsCollection = db.collection('reservations');
+    const reservationDocument = reservationsCollection.doc(reservationID);
+
+    const reservationDoc = await reservationDocument.get();
+    const reservationData = reservationDoc.data();
+
+    if ( !validateReservation(reservationData) )
+    {
+        throw Error("Reservation is not valid.");
+    }
+
+    const bikeType = reservationData['bikeType'];
+
+    const bikeID = await selectBike(bikeType);
+    const bikesCollection = db.collection('bikes');
+    const bikeDocument = bikesCollection.doc(bikeID);
+
+    // let reservationObject = {};
+    // reservationObject[`start.station`] = stationID;
+
+    await reservationDocument.update('status','unlocked');
+    await reservationDocument.update('start.station',stationID);
+    await reservationDocument.update('bike',bikeID);
+
+    await bikeDocument.update('status','unlocked');
+    await bikeDocument.update('reservation',reservationID);
+
+
+    return "success";
 };
 
-export const selectBike = (stationID, bikeType) => {
+
+export const validateReservation = (reservationData) =>
+{
+    console.log(reservationData);
+    return true;
+};
+
+export const removeBike = (stationID,bikeID) =>
+{
+
+};
+
+export const selectBike = async (stationID, bikeType) => {
     //TODO pick any bike of the appropriate bike type from the given station, return bikeID
-    return 0;
+    return "";
 };
 
-export const returnBike = (bikeID, stationID) => {
+export const returnBike = async (bikeID, stationID) => {
     //TODO return the given bike to the given station and mark the reservation as complete
     return 0;
 };
