27c90883ba9fb7d164fc537b2fbf8a2d74f1da80
George <hughesgeorgem@gmaill.com>
2019-02-28 20:16:21 +0000

    Implemented makeReport

--- src/firebase/reports.js
+++ src/firebase/reports.js
@@ -1,12 +1,35 @@
 import * as firebase from "firebase";
 
+import {getCurrentDateString, getCurrentTimeString} from "./time";
+import {makeTask} from "./tasks";
+
 
 //Make a customer report and attach it to a task, assigning that task to an operator
 export const makeReport = async (reservationID, category, comment) => {
-    //TODO: implement
+    //TODO: Test
 
-    //TODO: Create report
-    //TODO: Create task and attach
-    //TODO: Assign task
+    const theReport = {};
+
+    const auth = firebase.auth();
+    const uid = auth.currentUser.uid;
+
+    const db = firebase.firestore();
+    const reportsCollection = db.collection('reservations');
+
+
+    theReport['user'] = uid;
+    theReport['reservation'] = reservationID;
+    theReport['category'] = category;
+    theReport['comment'] = comment;
+    theReport['time']['date'] = getCurrentDateString();
+    theReport['time']['time'] = getCurrentTimeString();
+
+
+    const reportDocument = await reportsCollection.add(theReport);
+    const reportID = reportDocument.id;
+
+
+    await makeTask({reportID,category: "userReport"});
+
 };
 
