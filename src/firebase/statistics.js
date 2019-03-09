6edfe60f86ceee6ebbae8c75193ba33b338afe7f
George <hughesgeorgem@gmaill.com>
2019-03-09 21:12:54 +0000

    Added functions to be implemented for getting various statistics info

---
+++ src/firebase/statistics.js
@@ -0,0 +1,70 @@
+import * as firebase from "firebase";
+
+// timeScale Argument Rules:
+// 0 - Day
+// 1 - Week
+// 2 - Month
+// 3 - Year
+
+
+//Trips
+
+export const getNumberOfTripsCreated = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+export const getNumberOfBikesUnlocked = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+export const getNumberOfTripsCompleted = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+export const getNumberOfTripsCancelled = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+
+//Tasks
+
+export const getNumberOfTasksCreated = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+export const getNumberOfTasksCompleted = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+export const getNumberOfTasksReassigned = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+export const getNumberOfTasksExtended = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+
+//Report
+
+export const getNumberOfReportsCreated = async (timeScale) => {
+    //TODO: Implement
+
+};
+
+
+
+//Station
+
+export const getStationStatistics = async (timeScale) => {
+    //TODO: Implement
+
+};
