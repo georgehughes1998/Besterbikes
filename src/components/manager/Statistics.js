43601771060bc38d2a203d45161c96776d1df740
jackow98 <jw50@hw.ac.uk>
2019-03-15 10:45:55 +0000

    Statistics appear to return 0

--- src/components/manager/Statistics.js
+++ src/components/manager/Statistics.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import PageContainer from "../PageContainer";
-import {getStatistics} from '../../firebase/statistics'
-import Container from "semantic-ui-react/dist/commonjs/elements/Container/Container";
-import Header from "semantic-ui-react/dist/commonjs/elements/Header/Header";
-import Menu from "semantic-ui-react/dist/commonjs/collections/Menu/Menu";
-import StatisticSegment from "./StaticticSegment";
-import {SubmissionError} from "redux-form";
+import {getStatistics} from "../../firebase/statistics";
+import SubmissionError from "redux-form/es/SubmissionError";
 
 class Statistics extends React.Component{
 
     constructor(props) {
         super(props);
         this.state = {
-            statisticDMY: "d"
+            DMY: "daily",
+            authentication: {
+                signIn: ""
+            },
+            date: {},
+            reports: {},
+            reservation: {},
+            station: {},
+            task: {}
+
         };
     }
 
-    retrieveStatistic = async (statistic, year , month, day) => {
+    retrieveStatistic = async () => {
+        console.log("Retrieving stat");
 
-        console.log(statistic, year, month, day);
-        const obj = await getStatistics(statistic, year, month, day);
+        const obj = await getStatistics(["authentication_signIn"], 2019, 3, 12);
+
         if (obj) {
             console.log(obj);
-            console.log(obj[statistic]);
-            console.log(obj["authentication_signIn"]);
-            console.log(obj["authentication_signIn"]["2019"]["3"]["12"]);
-            this.setState({statistic: obj["authentication_signIn"]["2019"]["3"]["12"]});
+            console.log(obj["authentication_signIn"]["2019"]["3"]["14"]);
+            this.setState({authentication: {signIn : obj["authentication_signIn"]["2019"]["3"]["14"]}});
         } else {
             throw new SubmissionError({
                 _error: obj.message
@@ -33,50 +37,13 @@ class Statistics extends React.Component{
         }
     };
 
-    renderStatistic = (statistics, year, month, day) => {
-        return statistics.map((key, index) => {
-
-            const stat = this.retrieveStatistic(key, year, month, day);
-            console.log(key, stat);
-
-            return (
-                <StatisticSegment
-                    name = "Authentication"
-                    icon = "user"
-                    values = {
-                            {name: "Sign Ins", value: 12},
-                            {name: "Sign Ins", value: 245},
-                            {name: "Sign Ups", value: 1},
-                            {name: "Update Details", value: 2}
-                    }
-                />
-            )
-    })
-    };
-
     componentDidMount(){
-        this.retrieveStatistic(["authentication_signIn"], 2019, 3, 12)()
+        this.retrieveStatistic();
     }
 
     render(){
         return(
             <PageContainer>
-                <br/>
-                <Container textAlign='center'>
-                    <Header as={"h1"}>Statistics</Header>
-                </Container>
-                <br/>
-
-                <Menu pointing secondary widths={3}>
-                    <Menu.Item name="Daily" active={this.state.statisticDMY === "d"}
-                               onClick={() => this.setState({"statisticDMY": "d"})}/>
-                    <Menu.Item name="Monthly" active={this.state.statisticDMY === "m"}
-                               onClick={() => this.setState({"statisticDMY": "m"})}/>
-                    <Menu.Item name="Yearly" active={this.state.statisticDMY === "y"}
-                               onClick={() => this.setState({"statisticDMY": "y"})}/>
-                </Menu>
-
-                {this.renderStatistic(["authentication_signIn"], 2019, 3, 12)}
 
             </PageContainer>
         )
