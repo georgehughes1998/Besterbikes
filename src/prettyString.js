1cd85cb54250238021c17e59676ff551d1f82488
George <hughesgeorgem@gmaill.com>
2019-03-15 16:48:57 +0000

    getPrettyString function added for easily getting presentable text

---
+++ src/prettyString.js
@@ -0,0 +1,119 @@
+export const getPrettyString = (theString) =>
+{
+
+    switch (theString)
+    {
+        //Statistics
+        case "authentication_signUp":           return "Sign Ups";
+        case "authentication.signIn":           return "Sign Ins";
+        case "authentication.updateDetails":    return "Updated Details";
+
+        //Road bike make
+        case "reservation.stationHeriotWattUniversity.road.make":   return "Reservations of road bikes made at Heriot Watt University";
+        case "reservation.stationWaverley.road.make":   return "Reservations of road bikes made at Waverley";
+        case "reservation.stationHaymarket.road.make":   return "Reservations of road bikes made at Haymarket";
+        case "reservation.stationNationalMuseumOfScotland.road.make":   return "Reservations of road bikes made at National Museum of Scotland";
+        case "reservation.stationEdinburghZoo.road.make":   return "Reservations of road bikes made at Edinburgh Zoo";
+        case "reservation.stationEdinburghUniversityLibrary.road.make":   return "Reservations of road bikes made at Edinburgh University Library";
+        case "reservation.stationScottishParliament.road.make":   return "Reservations of road bikes made at the Scottish Parliament";
+        case "reservation.stationMerchistonCrossroads.road.make":   return "Reservations of road bikes made at Merchiston Crossroads";
+        case "reservation.stationRoyalBotanicGarden.road.make":   return "Reservations of road bikes made at the Royal Botanic Garden";
+        case "reservation.stationPortobelloBeachNorth.road.make":   return "Reservations of road bikes made at Portobello Beach North";
+        case "reservation.stationPortobelloBeachEast.road.make":   return "Reservations of road bikes made at Portobello Beach East";
+
+        //Mountain bike make
+        case "reservation.stationHeriotWattUniversity.mountain.make":   return "Reservations of mountain bikes made at Heriot Watt University";
+        case "reservation.stationWaverley.mountain.make":   return "Reservations of mountain bikes made at Waverley";
+        case "reservation.stationHaymarket.mountain.make":   return "Reservations of mountain bikes made at Haymarket";
+        case "reservation.stationNationalMuseumOfScotland.mountain.make":   return "Reservations of mountain bikes made at National Museum of Scotland";
+        case "reservation.stationEdinburghZoo.mountain.make":   return "Reservations of mountain bikes made at Edinburgh Zoo";
+        case "reservation.stationEdinburghUniversityLibrary.mountain.make":   return "Reservations of mountain bikes made at Edinburgh University Library";
+        case "reservation.stationScottishParliament.mountain.make":   return "Reservations of mountain bikes made at the Scottish Parliament";
+        case "reservation.stationMerchistonCrossroads.mountain.make":   return "Reservations of mountain bikes made at Merchiston Crossmountains";
+        case "reservation.stationRoyalBotanicGarden.mountain.make":   return "Reservations of mountain bikes made at the Royal Botanic Garden";
+        case "reservation.stationPortobelloBeachNorth.mountain.make":   return "Reservations of mountain bikes made at Portobello Beach North";
+        case "reservation.stationPortobelloBeachEast.mountain.make":   return "Reservations of mountain bikes made at Portobello Beach East";
+
+
+        //Road bike cancel
+        case "reservation.stationHeriotWattUniversity.road.cancel":   return "Reservations of road bikes cancelled at Heriot Watt University";
+        case "reservation.stationWaverley.road.cancel":   return "Reservations of road bikes cancelled at Waverley";
+        case "reservation.stationHaymarket.road.cancel":   return "Reservations of road bikes cancelled at Haymarket";
+        case "reservation.stationNationalMuseumOfScotland.road.cancel":   return "Reservations of road bikes cancelled at National Museum of Scotland";
+        case "reservation.stationEdinburghZoo.road.cancel":   return "Reservations of road bikes cancelled at Edinburgh Zoo";
+        case "reservation.stationEdinburghUniversityLibrary.road.cancel":   return "Reservations of road bikes cancelled at Edinburgh University Library";
+        case "reservation.stationScottishParliament.road.cancel":   return "Reservations of road bikes cancelled at the Scottish Parliament";
+        case "reservation.stationMerchistonCrossroads.road.cancel":   return "Reservations of road bikes cancelled at Merchiston Crossroads";
+        case "reservation.stationRoyalBotanicGarden.road.cancel":   return "Reservations of road bikes cancelled at the Royal Botanic Garden";
+        case "reservation.stationPortobelloBeachNorth.road.cancel":   return "Reservations of road bikes cancelled at Portobello Beach North";
+        case "reservation.stationPortobelloBeachEast.road.cancel":   return "Reservations of road bikes cancelled at Portobello Beach East";
+
+        //Mountain bike cancel
+        case "reservation.stationHeriotWattUniversity.mountain.cancel":   return "Reservations of mountain bikes cancelled at Heriot Watt University";
+        case "reservation.stationWaverley.mountain.cancel":   return "Reservations of mountain bikes cancelled at Waverley";
+        case "reservation.stationHaymarket.mountain.cancel":   return "Reservations of mountain bikes cancelled at Haymarket";
+        case "reservation.stationNationalMuseumOfScotland.mountain.cancel":   return "Reservations of mountain bikes cancelled at National Museum of Scotland";
+        case "reservation.stationEdinburghZoo.mountain.cancel":   return "Reservations of mountain bikes cancelled at Edinburgh Zoo";
+        case "reservation.stationEdinburghUniversityLibrary.mountain.cancel":   return "Reservations of mountain bikes cancelled at Edinburgh University Library";
+        case "reservation.stationScottishParliament.mountain.cancel":   return "Reservations of mountain bikes cancelled at the Scottish Parliament";
+        case "reservation.stationMerchistonCrossroads.mountain.cancel":   return "Reservations of mountain bikes cancelled at Merchiston Crossmountains";
+        case "reservation.stationRoyalBotanicGarden.mountain.cancel":   return "Reservations of mountain bikes cancelled at the Royal Botanic Garden";
+        case "reservation.stationPortobelloBeachNorth.mountain.cancel":   return "Reservations of mountain bikes cancelled at Portobello Beach North";
+        case "reservation.stationPortobelloBeachEast.mountain.cancel":   return "Reservations of mountain bikes cancelled at Portobello Beach East";
+
+
+
+
+        //Station unlock
+        case "reservation.stationHeriotWattUniversity.unlock":   return "Bikes unlocked at Heriot Watt University";
+        case "reservation.stationWaverley.unlock":   return "Bikes unlocked at Waverley";
+        case "reservation.stationHaymarket.unlock":   return "Bikes unlocked at Haymarket";
+        case "reservation.stationNationalMuseumOfScotland.unlock":   return "Bikes unlocked at National Museum of Scotland";
+        case "reservation.stationEdinburghZoo.unlock":   return "Bikes unlocked at Edinburgh Zoo";
+        case "reservation.stationEdinburghUniversityLibrary.unlock":   return "Bikes unlocked at Edinburgh University Library";
+        case "reservation.stationScottishParliament.unlock":   return "Bikes unlocked at the Scottish Parliament";
+        case "reservation.stationMerchistonCrossroads.unlock":   return "Bikes unlocked at Merchiston Crossmountains";
+        case "reservation.stationRoyalBotanicGarden.unlock":   return "Bikes unlocked at the Royal Botanic Garden";
+        case "reservation.stationPortobelloBeachNorth.unlock":   return "Bikes unlocked at Portobello Beach North";
+        case "reservation.stationPortobelloBeachEast.unlock":   return "Bikes unlocked at Portobello Beach East";
+
+        //Station return
+        case "reservation.stationHeriotWattUniversity.return":   return "Bikes returned at Heriot Watt University";
+        case "reservation.stationWaverley.return":   return "Bikes returned at Waverley";
+        case "reservation.stationHaymarket.return":   return "Bikes returned at Haymarket";
+        case "reservation.stationNationalMuseumOfScotland.return":   return "Bikes returned at National Museum of Scotland";
+        case "reservation.stationEdinburghZoo.return":   return "Bikes returned at Edinburgh Zoo";
+        case "reservation.stationEdinburghUniversityLibrary.return":   return "Bikes returned at Edinburgh University Library";
+        case "reservation.stationScottishParliament.return":   return "Bikes returned at the Scottish Parliament";
+        case "reservation.stationMerchistonCrossroads.return":   return "Bikes returned at Merchiston Crossmountains";
+        case "reservation.stationRoyalBotanicGarden.return":   return "Bikes returned at the Royal Botanic Garden";
+        case "reservation.stationPortobelloBeachNorth.return":   return "Bikes returned at Portobello Beach North";
+        case "reservation.stationPortobelloBeachEast.return":   return "Bikes returned at Portobello Beach East";
+
+
+        case "task.make":       return "Tasks Created";
+        case "task.complete":      return "Tasks Completed";
+        case "task.reassign":       return "Tasks Reassigned";
+        case "task.extend":       return "Tasks Extended";
+
+    }
+
+};
+
+// authentication.signUp
+// authentication.signIn
+//
+// reservation.[stationID].[bikeType].make
+// reservation.[stationID].[bikeType].cancel
+// reservation.update
+//
+// station.[stationID].unlock
+// station.[stationID].unlockOperator
+// station.[stationID].return
+//
+// task.make
+// task.complete
+// task.reassign
+// task.extend
+//
+// report.make
